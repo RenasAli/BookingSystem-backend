73c92648c1072bea3b0ef802404bdb1df60e8de8
chris <chri402s@stud.kea.dk>
2025-05-13 15:27:52 +0200

    Otp for backend, added confirmationMethod to frontend

--- src/dto/ResponseDto/publicCompanyResponse.ts
+++ src/dto/ResponseDto/publicCompanyResponse.ts
@@ -1,5 +1,6 @@
 import { AddressResponse } from "./AddressResponse";
 import { CompanyWorkdayResponse } from "./CompanyWorkdayResponse";
+import ConfirmationMethod from "../../model/enum/ConfirmationMethod";
 
 export interface PublicCompanyResponse {
     id: number;
@@ -8,6 +9,7 @@ export interface PublicCompanyResponse {
     phone: string;
     email: string;
     logo?: string;
+    confirmationMethod: ConfirmationMethod;
     address: AddressResponse;
     workday: Array<CompanyWorkdayResponse>
     services: Array<PublicServiceResponse>
